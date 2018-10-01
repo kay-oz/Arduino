31c048d03fe568468cd02dbeebca6c05b7dcb7c9
Kevin Mitchell <32917132+kay-oz@users.noreply.github.com>
2018-10-01 14:08:14 -0400

    Update iot-fw-connect2server.ino

--- iot-fw-connect2server/iot-fw-connect2server.ino
+++ iot-fw-connect2server/iot-fw-connect2server.ino
@@ -9,6 +9,8 @@
 #include <SPI.h>
 #include <DHT.h>
 #include <Nextion.h>
+#include <OneWire.h>
+
 float pHThreshold; //Holds the value of the initial Threshold
 float FinalpHThreshold;
 float LuxThreshold;
@@ -21,27 +23,44 @@ float t;
 float Rsensor; //Resistance of sensor in K
 static float pHValue, voltage; //review and determine usage.
 
+#define StartConvert 0
+#define ReadTemperature 1
 #define BLYNK_PRINT Serial
-#define DHTPIN 3                  // What digital pin we're connected to
 #define pHSensorPin A1            //pH meter Analog output to Arduino Analog Input 0
 #define Offset 0.00               //deviation compensate
 #define LED 6                     //Use this Pin to drive the Pump.
 #define TempsensorPin A0          //Temp meter Analog output to Arduino Analog Input 1
 #define LuxsensorPin A2           //Lux meter Analog output to Arduino Analog Input 2
 #define samplingInterval 20
-#define printInterval 1000
+#define printInterval 700
+#define tempSampleInterval 850
+#define AnalogSampleInterval 25
 #define ArrayLenth  40            //times of collection
 #define DHTTYPE DHT11             // DHT 11
 
 
+const byte numReadings = 20;                             //the number of sample times
+byte ECsensorPin = A4;                              //EC Meter analog output,pin on analog 4
+byte DS18B20_Pin = 2;                               //DS18B20 signal, pin on digital 2
+//unsigned int AnalogSampleInterval=25,printInterval=700,tempSampleInterval=850;  //analog sample interval;serial print interval;temperature sample interval
+unsigned int readings[numReadings];                         // the readings from the analog input
+byte index = 0;                                         // the index of the current reading
+unsigned long AnalogValueTotal = 0;                               // the running total
+unsigned int AnalogAverage = 0,averageVoltage=0;                       // the average
+unsigned long AnalogSampleTime,printTime,tempSampleTime;
+float temperature,ECcurrent; 
+
+//Temperature chip i/o
+OneWire ds(DS18B20_Pin);  // on digital pin 
+
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "INSERT YOUR BLYNK AUTH TOKEN";
+char auth[] = "69f2123ee5164cfe9665752a2bbe87e1";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "AP_NAME";
-char pass[] = "PASSWORD";
+char ssid[] = "Monitored WiFi";
+char pass[] = "Kmav1067$$";
 
 int pHArray[ArrayLenth];          //Store the average value of the sensor feedback
 int pHArrayIndex = 0;
@@ -52,21 +71,6 @@ int Luxsensor;
 SoftwareSerial nextion(10, 11);// Nextion TX11 to pin 2 and RX 11 to pin 3 of Arduino
 Nextion myNextion(nextion, 9600); //create a Nextion object named myNextion using the nextion serial port @ 9600bps
 
-DHT dht(DHTPIN, DHTTYPE);
-
-
-void sendSensor()
-{
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
-
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
- }
 
 
 void setup(void)
@@ -75,11 +79,17 @@ void setup(void)
   pHThreshold = 7.0;
   LuxThreshold = 100;
   TempThreshold = 69;
+  Serial.begin(9600);
+  for (byte thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;
+  TempProcess(StartConvert);   //let the DS18B20 start the convert
+  AnalogSampleTime=millis();
+  printTime=millis();
+  tempSampleTime=millis();
   pinMode(LED, OUTPUT);
   digitalWrite(6, LOW);
-  Serial.begin(9600);
   myNextion.init(); // send the initialization commands for Page 0
-  Blynk.begin(auth, "iot-gardens.blynk.cc");
+  Blynk.begin(auth);
  }
 
 
@@ -88,15 +98,80 @@ void loop(void)
 {
   publishResults();
   SetpHThreshold();
-  TempSensor();
+  //TempSensor();
   LuxReading();
   pHMeasurementValue();
-  //Blynk.run();
+  Blynk.run();
+  ECReading();
   //timer.run();
   //WiFiConnection();
 }
 
+void ECReading() {
+  /*
+   Every once in a while,sample the analog value and calculate the average.
+  */
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  {
+    AnalogSampleTime=millis();
+     // subtract the last reading:
+    AnalogValueTotal = AnalogValueTotal - readings[index];
+    // read from the sensor:
+    readings[index] = analogRead(ECsensorPin);
+    // add the reading to the total:
+    AnalogValueTotal = AnalogValueTotal + readings[index];
+    // advance to the next position in the array:
+    index = index + 1;
+    // if we're at the end of the array...
+    if (index >= numReadings)
+    // ...wrap around to the beginning:
+    index = 0;
+    // calculate the average:
+    AnalogAverage = AnalogValueTotal / numReadings;
+  }
+  /*
+   Every once in a while,MCU read the temperature from the DS18B20 and then let the DS18B20 start the convert.
+   Attention:The interval between start the convert and read the temperature should be greater than 750 millisecond,or the temperature is not accurate!
+  */
+   if(millis()-tempSampleTime>=tempSampleInterval) 
+  {
+    tempSampleTime=millis();
+    temperature = TempProcess(ReadTemperature)*1.8+32;  // read the current temperature from the  DS18B20
+    TempProcess(StartConvert);                   //after the reading,start the convert for next reading
+    
+  }
+   /*
+   Every once in a while,print the information on the serial monitor.
+  */
+  if(millis()-printTime>=printInterval)
+  {
+    printTime=millis();
+    averageVoltage=AnalogAverage*(float)5000/1024;
+    Serial.print("Analog value:");
+    Serial.print(AnalogAverage);   //analog average,from 0 to 1023
+    Serial.print("    Voltage:");
+    Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
+    Serial.print("mV    ");
+    Serial.print("temp:");
+    Serial.print(temperature);    //current temperature
+    Serial.print("^C     EC:");
+    
+    float TempCoefficient=temperature*1.8+32;    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
+    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    else
+    { 
+      if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
+      else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
+      else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
+      ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(ECcurrent,2);  //two decimal
+      Serial.println("ms/cm");
+    }
+  }
 
+}
 
 void pHMeasurementValue() {
   static unsigned long samplingTime = millis();
@@ -210,11 +285,11 @@ void SetpHThreshold()
 
 
 //Temperature Sensor Reading
-
+/*
 void TempSensor() {
  /* int a = analogRead(TempsensorPin);
   float R = 1023.0/((float)a)-1.0;
-  R = 100000.0*R;;*/
+  R = 100000.0*R;;
 
   TempsensorValue= dht.readTemperature(); //convert to temperature via datasheet ;
   farh = dht.readTemperature(true);
@@ -222,6 +297,7 @@ void TempSensor() {
 
 }
 
+*/
 
 void LuxReading() {
   LuxsensorValue = analogRead(LuxsensorPin);
@@ -241,19 +317,60 @@ void publishResults() {
     myNextion.setComponentText("Luxb", String(LuxsensorValue));
     myNextion.setComponentText("pH", String(pHValue));
     myNextion.setComponentText("pHb", String(pHValue));
-    myNextion.setComponentText("Temp", String(farh));
-    myNextion.setComponentText("Tempb", String(TempsensorValue));
-    myNextion.setComponentText("Unit", String("F"));
-    myNextion.setComponentText("Unitb", String("C"));
+    myNextion.setComponentText("Temp", String(temperature));
+    myNextion.setComponentText("Tempb", String(ECcurrent));
+    myNextion.setComponentText("Unit", String("F "));
+    myNextion.setComponentText("Unitb", String("EC"));
     delay(250);
-    Blynk.virtualWrite(V2,h);
-    Blynk.virtualWrite(V1,farh);
+    Blynk.virtualWrite(V1,ECcurrent);
     Blynk.virtualWrite(V3,pHValue);
     Blynk.virtualWrite(V4,LuxsensorValue);
+    Blynk.virtualWrite(V5,temperature);
   }
 }
 
-
+/*
+ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
+*/
+float TempProcess(bool ch)
+{
+  //returns the temperature from one DS18B20 in DEG Celsius
+  static byte data[12];
+  static byte addr[8];
+  static float TemperatureSum;
+  if(!ch){
+          if ( !ds.search(addr)) {
+              Serial.println("no more sensors on chain, reset search!");
+              ds.reset_search();
+              return 0;
+          }      
+          if ( OneWire::crc8( addr, 7) != addr[7]) {
+              Serial.println("CRC is not valid!");
+              return 0;
+          }        
+          if ( addr[0] != 0x10 && addr[0] != 0x28) {
+              Serial.print("Device is not recognized!");
+              return 0;
+          }      
+          ds.reset();
+          ds.select(addr);
+          ds.write(0x44,1); // start conversion, with parasite power on at the end
+  }
+  else{  
+          byte present = ds.reset();
+          ds.select(addr);    
+          ds.write(0xBE); // Read Scratchpad            
+          for (int i = 0; i < 9; i++) { // we need 9 bytes
+            data[i] = ds.read();
+          }         
+          ds.reset_search();           
+          byte MSB = data[1];
+          byte LSB = data[0];        
+          float tempRead = ((MSB << 8) | LSB); //using two's compliment
+          TemperatureSum = tempRead / 16;
+    }
+          return TemperatureSum;  
+}
 
 
 /*
@@ -304,3 +421,4 @@ void PWConnect() {
   }
 }
 */
+
